663f64e507852ac57d8aa54046d9f1111d80bab6
朱彤枫 <zhutongfeng@xdf.cn>
2019-05-28 18:02:35 +0800

    在公司update

--- src/main/java/com/zhuoyao/scanyaoling/client.java
+++ src/main/java/com/zhuoyao/scanyaoling/client.java
@@ -2,17 +2,23 @@ package com.zhuoyao.scanyaoling;
 
 import com.alibaba.fastjson.JSONObject;
 import com.zhuoyao.vo.WssVo;
+import org.apache.commons.lang.ArrayUtils;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.io.FileReader;
 import java.net.URI;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 public class client {
     public static void main(String[] args) {
         try {
             /**
-             * js中的加密过程
+             *
              * var e = function(e) {
              *     for (var n = new ArrayBuffer(2 * e.length), r = new Uint16Array(n), t = 0, o = e.length; t < o; t++) r[t] = e.charCodeAt(t);
              *     return r;
@@ -23,22 +29,24 @@ public class client {
              *     return s.set(new Uint8Array(o), 0), s.set(r, 4), console.log(s, s.buffer, t), s.buffer;
              * };
              */
-            
+
             // 这里用的binance的socket接口，国内调用需要VPN，使用换成你的就行
 //            String url = "wss://stream.binance.com:9443/ws/ethbtc@ticker";
 //            String url = "wss://stream.binance.com:9443/ws/ethbtc@depth20";
             String url = "wss://publicld.gwgo.qq.com?account_value=0&account_type=0&appid=0&token=0";
             URI uri = new URI(url);
-            String currentversion="sprite_0e4ebf1344bf35582f7504ee265f32eb.json";
-            String version="v0.9.512.2316";
-
-            WssVo wv=new WssVo();
+//            String currentversion = "sprite_0e4ebf1344bf35582f7504ee265f32eb.json";
+//            String version = "v0.9.512.2316";
+//
+            WssVo wv = new WssVo();
             wv.setRequest_type("1001");
             wv.setLongtitude("39.954474285439886");
             wv.setLatitude("116.32746934890747");
             wv.setRequestid("1001");
             wv.setPlatform("0");
-            WebSocketClient mWs = new WebSocketClient(uri){
+            System.out.println(JSONObject.toJSONString(wv));
+            System.out.println(JSONObject.toJSONString(wv).length());
+            WebSocketClient mWs = new WebSocketClient(uri) {
                 @Override
                 public void onOpen(ServerHandshake serverHandshake) {
 
@@ -59,12 +67,46 @@ public class client {
 
                 }
             };
-            mWs.connect();
+//            mWs.connect();
+//
+//            Thread.sleep(5000);
+//            ScriptEngineManager sem = new ScriptEngineManager();
+//            ScriptEngine engine = sem.getEngineByName("js");
+//            String js;
+//
+//
+//            js = "var names = [\"john\", \"jerry\", \"bob\"]\n";
+//            js += "var a = names[0];\n";
+//            js += "print(a)";
+//
+//            engine.eval(js);
 
-            Thread.sleep(5000);
+//            String js2 = "";
+//            engine.eval(js2);
+//            String jsName = "F:\\ideaProject\\bot\\src\\main\\java\\com\\zhuoyao\\scanyaoling\\EncryptMessage.js";
+////读取js
+//            FileReader fileReader = new FileReader(jsName);
+////执行指定脚本
+//            engine.eval(fileReader);
+//            if (engine instanceof Invocable) {
+//                Invocable in = (Invocable) engine;
+//
+////                System.out.println(in.invokeFunction("json2buffer", JSONObject.toJSONString(wv),JSONObject.toJSONString(wv).length()));
+////                Object o=in.invokeFunction("json2buffer", JSONObject.toJSONString(wv),JSONObject.toJSONString(wv).length());
+//                System.out.println("11111");
+//                System.out.println("11111");
+//                System.out.println("11111");
+//                byte[] bb=JSONObject.toJSONString(wv).getBytes();
+//                byte[] bbb={0,0,0,123};
+//                byte[] result=ArrayUtils.addAll(bbb,bb);
+////                byte[] aa=(byte[])(in.invokeFunction("json2buffer", JSONObject.toJSONString(wv),JSONObject.toJSONString(wv).length()));
+//                mWs.send(result);
 
-                System.out.println("haha");
-            mWs.send("123");
+//            }
+
+//            System.out.println("haha");
+
+//            mWs.send("123");
         } catch (Exception e) {
             e.printStackTrace();
         }
